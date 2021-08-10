d8b35f237ceb9cb4784d4bb5ef375a4836e99ea9
Maximilinano Osorio <mosorio@isi.edu>
2021-08-10 17:44:00 -0400

    fix: add example

--- utils/netcdf.py
+++ utils/netcdf.py
@@ -1,3 +1,5 @@
+
+
 def open(file_name: str):
     """Open a Netcdf 
 
@@ -10,6 +12,7 @@ def open(file_name: str):
     return data
 
 def get_spatial_info(data):
+    #  Example output  {"type":"BoundingBox","value":{"xmax":"59.875","xmin":"10.125","ymax":"29.875","ymin":"-14.875"}}
     return  
 
 def get_temporal_info(data):
