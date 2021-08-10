19c481e04754d8ddd35d4667ee9eaf3ce3a7fcab
Harshita <hbhorshetti@gmail.com>
2021-08-10 15:30:40 -0700

    code to open dataset and get spatial data

--- utils/netcdf.py
+++ utils/netcdf.py
@@ -1,4 +1,4 @@
-
+import xarray as xr
 
 def open(file_name: str):
     """Open a Netcdf 
@@ -9,17 +9,31 @@ def open(file_name: str):
     Returns:
         [type]: [description]
     """
+    data=xr.open_dataset(file_name)
     return data
 
 def get_spatial_info(data):
     #  Example output  {"type":"BoundingBox","value":{"xmax":"59.875","xmin":"10.125","ymax":"29.875","ymin":"-14.875"}}
-    return  
+    spatial_data = data.geospatial_bounds
+    spatial_dict = dict()
+    spatial_dict['type'] = 'BoundingBox'
+    xmax, xmin, ymax, ymin = [float(x) for x in spatial_data]
+    spatial_dict['value'] = {
+        'xmax': xmax,
+        'xmin': xmin,
+        'ymax': ymax,
+        'ymin': ymin
+    }
+    print(spatial_dict)
+    return spatial_dict
 
 def get_temporal_info(data):
     return
 
 
 if __name__ == '__main__':
-    data = open()
-    get_spatial_info(data)
+    PATH = '../../Topoflow_Galana/Test1_2D-Q_1.nc'
+    data = open(PATH)
+    print(data)
+    spatial = get_spatial_info(data)
     get_temporal_info(data)
