058a0c1c25758418c2b6f94a6d092ea552d64683
Maximilinano Osorio <mosorio@isi.edu>
2021-08-10 17:32:59 -0400

    fix: skeleton netcdf

---
+++ utils/netcdf.py
@@ -0,0 +1,22 @@
+def open(file_name: str):
+    """Open a Netcdf 
+
+    Args:
+        file_name (str): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    return data
+
+def get_spatial_info(data):
+    return  
+
+def get_temporal_info(data):
+    return
+
+
+if __name__ == '__main__':
+    data = open()
+    get_spatial_info(data)
+    get_temporal_info(data)
