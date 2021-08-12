4101249d423c2b6a44f32402306c787b5660a733
Maximilinano Osorio <mosorio@isi.edu>
2021-08-12 15:19:52 -0400

    fix: tests

--- tests/test_netcdf.py
+++ tests/test_netcdf.py
@@ -5,12 +5,12 @@ RESOURCES = "resources"
 resource_dir = Path(__file__).parent / RESOURCES
 
 def test_open():
-    data = open(resource_dir / "Test1_2D-d")
+    data = open(resource_dir / "Test1_2D-d.nc")
     assert data
 
 
 def test_spatial_info():
-    data = open(resource_dir / "Test1_2D-d")
+    data = open(resource_dir / "Test1_2D-d.nc")
     geo_spatial = get_spatial_info(data)
     geo_spatial_expected = {"type":"BoundingBox","value":{"xmax":"36.4","xmin":"-3.14333333","ymax":"40.76666667","ymin":"0.69"}}
     assert geo_spatial == geo_spatial_expected
