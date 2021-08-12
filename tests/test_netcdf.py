dcebb8455602623ad425872265fef52064863642
Maximiliano Osorio <maxiosorio@gmail.com>
2021-08-12 15:39:27 -0400

    Adding tests and setup install (#4)

--- tests/test_netcdf.py
+++ tests/test_netcdf.py
@@ -1,16 +1,24 @@
-from utils.netcdf import get_spatial_info, open
+from dcatregister.netcdf import get_spatial_info, open_dataset
 from pathlib import Path
 
 RESOURCES = "resources"
 resource_dir = Path(__file__).parent / RESOURCES
 
 def test_open():
-    data = open(resource_dir / "Test1_2D-d")
+    data = open_dataset(resource_dir / "Test1_2D-d.nc")
     assert data
 
 
 def test_spatial_info():
-    data = open(resource_dir / "Test1_2D-d")
+    data = open_dataset(resource_dir / "Test1_2D-d.nc")
     geo_spatial = get_spatial_info(data)
-    geo_spatial_expected = {"type":"BoundingBox","value":{"xmax":"36.4","xmin":"-3.14333333","ymax":"40.76666667","ymin":"0.69"}}
+    geo_spatial_expected = {
+        "type":"BoundingBox",
+        "value":{
+            "xmax": 36.4,
+            "xmin": -3.143333333333,
+            "ymax": 40.766666666667,
+            "ymin": 0.69
+            }
+        }
     assert geo_spatial == geo_spatial_expected
