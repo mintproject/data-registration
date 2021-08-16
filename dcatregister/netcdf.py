f8d250f94c69b54a99fbb4c54ed1730343a400d0
Harshita <hbhorshetti@gmail.com>
2021-08-16 10:09:56 -0700

    update temporal data info

--- dcatregister/netcdf.py
+++ dcatregister/netcdf.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import datetime
 
 def open_dataset(file_name: str):
     """Open a Netcdf 
@@ -27,12 +28,20 @@ def get_spatial_info(data):
     return spatial_dict
 
 def get_temporal_info(data):
-    return
+    t = dict()
+    date_s = str(data.datetime[0]).split('\'')
+    date_e = str(data.datetime[-1]).split('\'')
+    date_start = datetime.datetime.strptime(date_s[3], "%Y-%m-%d %H:%M:%S")
+    date_end = datetime.datetime.strptime(date_e[3], "%Y-%m-%d %H:%M:%S")
+    t['start_time'] = date_start.isoformat()
+    t['end_time'] = date_end.isoformat()
+    return t
 
 
 if __name__ == '__main__':
     PATH = '../../Topoflow_Galana/Test1_2D-Q_1.nc'
-    data = open(PATH)
+    data = open_dataset(PATH)
     print(data)
     spatial = get_spatial_info(data)
-    get_temporal_info(data)
+    temporal = get_temporal_info(data)
+    print(temporal)
